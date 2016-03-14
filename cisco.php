03fbd24c69122274de43170b035ac8962dc429a5
Konstantin Shalygin <k0ste@k0ste.ru>
2016-03-14 13:18:18 +0600

    Use mysqli instead mysql. PHP 7 Support

--- cisco.php
+++ cisco.php
@@ -10,14 +10,12 @@ else {
   $model = $_GET['model'];
 }
 
-$link = mysql_connect($mysql_host, $mysql_user, $mysql_pwd);
+$link = mysqli_connect($mysql_host, $mysql_user, $mysql_pwd, $mysql_db);
 
 if (!$link)
-  exit('Error (' . mysql_errno() . '): ' . mysql_error(). "\n");
-if (!mysql_select_db($mysql_db, $link))
-  exit('Error (' . mysql_errno() . '): ' . mysql_error(). "\n");
+  exit('Error (' . mysqli_errno() . '): ' . mysqli_error(). "\n");
 
-mysql_query("SET NAMES 'utf8'");
+mysqli_query($link, "SET NAMES 'utf8'");
 
 $query = sprintf("
   SELECT
@@ -25,28 +23,28 @@ $query = sprintf("
     spa.mac AS mac
   FROM
     cts_pbx_accounts spa
-  WHERE spa.mac='%s'", mysql_real_escape_string($_GET['mac'], $link));
+  WHERE spa.mac='%s'", mysqli_real_escape_string($link, $_GET['mac']));
 
-$result = mysql_query($query);
+$result = mysqli_query($link, $query);
 
 if($result == 0) {
   $channel = '0';
   $pass = '0';
   $state = 'WARNING (bad query)';
 }
-else if(($num_rows = mysql_num_rows($result)) == 0) {
+else if(($num_rows = mysqli_num_rows($result)) == 0) {
   $channel = '0';
   $pass = '0';
   $state = 'CRITICAL (no chan found)';
 }
-else while($row = mysql_fetch_array($result)) {
+else while($row = mysqli_fetch_array($result)) {
   $channel = $row['chan'];
   $pass = $row['mac'];
   $state = 'OK (chan found)';
 }
 
-mysql_free_result($result);
-mysql_close($link);
+mysqli_free_result($result);
+mysqli_close($link);
 
 if($model == 'SPA514G') {
   $upgrade_rule = $upgrade_rule_SPA51x;
